99b64a9eae7f6a370b2d653b2fc66f722344b61e
crazyyy <crazyyy@gmail.com>
2015-09-16 01:45:17 +0300

    programm

--- wordpress/wp-content/themes/wp-solidarnost/loop.php
+++ wordpress/wp-content/themes/wp-solidarnost/loop.php
@@ -1,7 +1,7 @@
 <?php if (have_posts()): while (have_posts()) : the_post(); ?>
   <div id="post-<?php the_ID(); ?>" <?php post_class('looper'); ?>>
 
-    <h2 class="inner-title">
+    <h2>
       <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
     </h2><!-- /post title -->
 
--- wordpress/wp-content/themes/wp-solidarnost/page-bio.php
+++ wordpress/wp-content/themes/wp-solidarnost/page-bio.php
@@ -1,4 +1,4 @@
-<?php /* Template Name: Demo Page Template */ get_header(); ?>
+<?php /* Template Name: Bio Page Template */ get_header(); ?>
   <?php if (have_posts()): while (have_posts()) : the_post(); ?>
     <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
@@ -14,5 +14,5 @@
 
     </article>
   <?php endif; ?>
-<?php get_sidebar(); ?>
+
 <?php get_footer(); ?>
